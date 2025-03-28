1d6295ef7bcf982e1bc1e587ed13a357f7efa33e
Pikula <tomasz.pikula@accenture.com>
2025-03-28 21:45:07 +0100

    firewall policy

--- modules/updateFirewallPolicy/variables.tf
+++ modules/updateFirewallPolicy/variables.tf
@@ -8,6 +8,7 @@ variable "resource_group_name" {
 variable "firewall_policy_names" {
   type = map(object({
     name = string
+    location = string
     # tls_inspection_key_vault_secret_id = string
     intrusion_detection_mode = string
     intrusion_detection_traffic_bypass = list(object({
