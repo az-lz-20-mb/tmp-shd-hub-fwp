a2f2fe6ee29ba17c3caf7e073c8b97d2b67d17fe
Pikula <tomasz.pikula@accenture.com>
2025-03-28 21:16:30 +0100

    firewall policy

--- modules/updateFirewallPolicy/main.tf
+++ modules/updateFirewallPolicy/main.tf
@@ -3,13 +3,13 @@ module "firewall_policy" {
   for_each = var.firewall_policy_names  # Expecting a map of policy names
 
   name                = each.value.name  # Use the name from the map
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = lookup(var.resource_group_name[each.value.location], "location") 
+  resource_group_name = lookup(var.resource_group_name[each.value.location], "rg") 
 
   # Attach TLS Inspection - conditionally based on variable
-  tls_inspection {
-    key_vault_secret_id = each.value.tls_inspection_key_vault_secret_id
-  }
+  # tls_inspection {
+  #   key_vault_secret_id = each.value.tls_inspection_key_vault_secret_id
+  # }
 
   # Enable Intrusion Detection (IDPS)
   intrusion_detection {
--- modules/updateFirewallPolicy/variables.tf
+++ modules/updateFirewallPolicy/variables.tf
@@ -1,8 +1,3 @@
-variable "location" {
-  type        = string
-  description = "The Azure region to deploy resources into."
-}
-
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group."
@@ -11,7 +6,7 @@ variable "resource_group_name" {
 variable "firewall_policy_names" {
   type = map(object({
     name = string
-    tls_inspection_key_vault_secret_id = string
+    # tls_inspection_key_vault_secret_id = string
     intrusion_detection_mode = string
     intrusion_detection_traffic_bypass = list(object({
       name = string
